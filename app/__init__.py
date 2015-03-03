84bc35446eb58de6a5cadba072fa6d4f99af6cd5
justus <justus@Notebook>
2015-03-03 10:02:11 +0700

    try out again fro app.config

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 __author__ = 'justus'
 
 from flask import Flask
+import os
 
 app = Flask(__name__, static_path='')
 from app import views
 from app import config_page
+
+app.config.from_object(os.environ['APP_SETTINGS'])
