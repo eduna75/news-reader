edd004a362ad39cb6ea51fa18d8ed08bf9f4fd93
eduna75 <%vr3*mof[NK{;x@/nf3I>
2015-04-10 23:20:12 +0700

    fixed post_generator

--- app/post_generator.py
+++ app/post_generator.py
@@ -4,33 +4,6 @@ import feedparser
 from app.db_connect import DBConnect as DBc
 
 
-def generator():
-    link = DBc.db_connect()
-    links = link.execute('SELECT * FROM url')
-    total_committed = 0
-    total_double = 0
-    for l in links:
-        url_id = l[0]
-        feeds = feedparser.parse(l[1])
-        for i in xrange(0, len(feeds['entries'])):
-            try:
-                post = [feeds['entries'][i].title, feeds['entries'][i].summary, feeds['entries'][i].link,
-                        feeds['feed'].title, url_id, feeds['entries'][i].published]
-                print post, '\n'
-            except BaseException as e:
-                print 'could not extract everything', e
-            try:
-                link.execute('INSERT INTO news(title, summary, link, publisher, source, date) VALUES (?,?,?,?,?,?);',
-                             post)
-                link.commit()
-                total_committed += 1
-            except BaseException as e:
-                print 'Double entry, was not committed to the database', e
-                total_double += 1
-    return total_committed
-    link.close()
-
-
 def time_ago(data):
     t = time.localtime()
     p = data
@@ -40,7 +13,7 @@ def time_ago(data):
 
 def fetch(urls):
     feed = []
-    for links in urls:
+    for links in urls[0:2]:
         feeds = feedparser.parse(links.url)
         for i in xrange(0, len(feeds.entries)):
             entries = {'title': feeds.entries[i].title, 'summary': feeds.entries[i].summary,
